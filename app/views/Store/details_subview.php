00a51e214c6d478304d1581455a7132486b35ccf
danichh <hangdanich@hotmail.com>
2022-03-17 10:30:21 -0400

    Debug Store/detail_subvie

--- app/views/Store/details_subview.php
+++ app/views/Store/details_subview.php
@@ -1,9 +1,8 @@
-<h1>Store details</h1>
-	<form method='post' action=''>
-		<label class='form-label'>Store name:<input type='text' name='store_name' class='form-control' value='<?= $data->store_name?>' /></label><br>
-		<label class='form-label'>Store address:<input type='text' name='store_address' class='form-control' value='<?= $data->store_address?>' /></label><br>
-		<label class='form-label'>Description:<textarea name='notes' class='form-control'><?= $data->description?></textarea></label><br>
-    </form>
-    <?php   
-        $this->view('subviews/products', $data->getProducts($data->store_id));
-    ?>  
+
+  <h1>Store details</h1>
+            <form method='post' action=''>
+                <label class='form-label'>Store name:<input  disabled type='text' name='store_name' class='form-control' value='<?= $data->store_name?>' /></label><br>
+                <label class='form-label'>Store address:<input disabled type='text' name='store_address' class='form-control' value='<?= $data->store_address?>' /></label><br>
+                <label class='form-label'>Store description:<textarea disabled type='text' name='description'  cols="80" class='form-control' > <?= $data->description?></textarea></label><br>
+            </form>
+    
--- app/views/Store/index.php
+++ app/views/Store/index.php
@@ -8,21 +8,21 @@
     </head>
     <body>
         <?php
-
             $this->view('subviews/navigation');
         ?>
+        
         <div class='container'>
-
-            <h1>Store details</h1>
-            <form method='post' action=''>
-                <label class='form-label'>Store name:<input type='text' name='store_name' class='form-control' value='<?= $data->store_name?>' /></label><br>
-                <label class='form-label'>Store address:<input type='text' name='store_address' class='form-control' value='<?= $data->store_address?>' /></label><br>
-                <label class='form-label'>Store description:<input type='text' name='description' class='form-control' value='<?= $data->description?>' /></label><br>
-            </form>
-                
             <?php
-                if (isset($_SESSION['store_id']) && $_SESSION['store_id'] == $data->store_id)
-                    echo "<li><a href='/Product/create/$data->store_id'>Add an item</a></li>  <br>"; //do check for this and make sure only owner is allowed
+                $this->view('Store/details_subview', $data);
+            ?>
+    
+            <?php
+                if (isset($_SESSION['store_id']) && $_SESSION['store_id'] == $data->store_id){
+                    echo "<li><a href='/Product/create/$data->store_id'>Add an item</a></li>  <br>";
+                    echo "<li><a href='/Store/update/$data->store_id'>update</a></li>  <br>";
+                    echo "<li><a href='/Store/delete/$data->store_id'>Delete</a></li>  <br>";
+                }
+                     //do check for this and make sure only owner is allowed
                 $products = $data->getProducts($data->store_id);
                 $this->view('subviews/products', $products); //implement this
             ?>
