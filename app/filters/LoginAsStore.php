0f2afd6985f9cb98c8c69ee61f5e0c934e039ec3
gbouzon <ga@bouzon.com.br>
2022-03-19 01:47:44 -0400

    fixed access filtering

--- app/filters/LoginAsStore.php
+++ app/filters/LoginAsStore.php
@@ -5,9 +5,15 @@
         class LoginAsStore {
 
             function execute() {
-                if (!isset($_SESSION['store_id'] || $_SESSION['store_id']  )) { //user is not logged in
-                    header('location:/Store/create');
-                    return true; //I want to indicate to the framework that the user is filtered
+                if (!isset($_SESSION['store_id'])) { //if store_id isn't set -> not a store 
+                    if (isset($_SESSION['user_id'])) { //if user is set -> logged in as user
+                        header('location:/Store/create/' . $_SESSION['user_id']); //redirecting to create a store
+                        return true; 
+                    }
+                    else {
+                        header('location:/User/register'); //if not logged in as user -> redirecting to register
+                        return true;
+                    }
                 }
                 return false;
             }
