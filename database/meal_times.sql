3e3ca0b1b9a91800eae8742331eab3c176eaeb84
Giuliana Bouzon <ga@bouzon.com.br>
2022-04-25 15:14:15 -0400

    updated database

--- database/meal_times.sql
+++ database/meal_times.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 25, 2022 at 09:19 AM
+-- Generation Time: Apr 25, 2022 at 09:12 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
@@ -46,7 +46,7 @@ INSERT INTO `order` (`order_id`, `user_id`, `store_id`, `total`, `createAt`, `or
 (3, 8, 1, NULL, '2022-03-20 16:48:55', 0),
 (4, 15, 4, NULL, '2022-03-22 04:03:17', 0),
 (5, 3, 1, NULL, '2022-03-21 02:52:17', 0),
-(6, 12, 1, NULL, '2022-04-18 17:22:23', 1),
+(6, 12, 1, NULL, '2022-04-25 07:56:19', 4),
 (7, 10, NULL, NULL, '2022-03-21 03:37:45', 0),
 (8, 15, NULL, NULL, '2022-03-21 03:38:00', 0),
 (9, 15, NULL, NULL, '2022-03-21 04:39:12', 0),
@@ -57,7 +57,19 @@ INSERT INTO `order` (`order_id`, `user_id`, `store_id`, `total`, `createAt`, `or
 (14, 7, NULL, NULL, '2022-04-25 03:15:55', 0),
 (15, 5, 1, NULL, '2022-04-25 07:01:43', 0),
 (16, 7, NULL, NULL, '2022-04-25 06:30:15', 0),
-(17, 5, NULL, NULL, '2022-04-25 06:54:54', 0);
+(17, 5, NULL, NULL, '2022-04-25 06:54:54', 0),
+(18, 7, NULL, NULL, '2022-04-25 07:32:20', 0),
+(19, 5, NULL, NULL, '2022-04-25 08:20:43', 0),
+(20, 5, NULL, NULL, '2022-04-25 08:22:31', 0),
+(21, 5, NULL, NULL, '2022-04-25 08:31:01', 0),
+(22, 7, NULL, NULL, '2022-04-25 08:36:06', 0),
+(23, 7, NULL, NULL, '2022-04-25 08:41:00', 0),
+(24, 5, NULL, NULL, '2022-04-25 13:45:54', 0),
+(29, 5, NULL, NULL, '2022-04-25 17:50:49', 0),
+(33, 5, NULL, NULL, '2022-04-25 18:40:43', 0),
+(34, 5, NULL, NULL, '2022-04-25 19:00:12', 0),
+(35, 5, NULL, NULL, '2022-04-25 19:01:32', 0),
+(36, 6, NULL, NULL, '2022-04-25 19:06:12', 0);
 
 -- --------------------------------------------------------
 
@@ -79,7 +91,6 @@ CREATE TABLE `order_detail` (
 --
 
 INSERT INTO `order_detail` (`order_detail_id`, `order_id`, `product_id`, `quantity`, `unit_price`, `price`) VALUES
-(112, 6, 1, 2, 3.5, 7),
 (114, 6, 2, 1, 3.5, 3.5),
 (115, 12, 5, 2, 13.33, 26.66),
 (120, 15, 2, 2, 3.5, 7);
@@ -94,7 +105,7 @@ CREATE TABLE `product` (
   `product_id` int(11) NOT NULL,
   `store_id` int(11) NOT NULL,
   `product_name` varchar(30) NOT NULL,
-  `product_image` varchar(50) DEFAULT 'blank.png',
+  `product_image` varchar(50) DEFAULT 'blank.jpg',
   `product_availability` tinyint(4) NOT NULL DEFAULT 1 COMMENT 'product is generally available when it''s added. 0 means sold out and 1 means available',
   `product_price` double NOT NULL,
   `product_description` text NOT NULL
@@ -105,12 +116,11 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`product_id`, `store_id`, `product_name`, `product_image`, `product_availability`, `product_price`, `product_description`) VALUES
-(1, 1, 'Vanilla Cupcake', 'blank.png', 0, 3.5, 'A simple Vanilla cupcake!'),
-(2, 1, 'Strawberry cupcake', 'blank.png', 1, 3.5, 'A Strawberry cupcake!'),
-(5, 3, 'Cheese Pizza ', 'blank.png', 1, 13.33, 'This is a Chesse Pizza'),
-(6, 5, 'Customer2 Store item 0', 'blank.png', 1, 9.99, 'Customer2 Store item'),
-(8, 3, 'Pepperoni pizza ', '6235ff762f7c0.jpg', 1, 16, 'This is a pepperoni Pizza'),
-(9, 4, 'Poutine', '62377897f40b4.jpg', 1, 16, 'This a an poutine !'),
+(1, 1, '', 'v.jpg', 0, 0, ''),
+(2, 1, 'Strawberry cupcake', '62370d6f27ce7.jpg', 1, 3.5, 'A Strawberry cupcake!'),
+(5, 3, 'Cheese Pizza ', 'cheese-pizza.jpg', 1, 13.33, 'This is a Chesse Pizza'),
+(8, 3, 'Pepperoni pizza ', '62376049d61b0.jpg', 1, 16, 'This is a pepperoni Pizza'),
+(9, 4, 'Poutine', '623778e2bb0d8.jpg', 1, 16, 'This a an poutine !'),
 (10, 4, 'Rouge Bol', '623778d1cd398.jpg', 1, 14, 'This a Bol');
 
 -- --------------------------------------------------------
@@ -162,9 +172,9 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`user_id`, `user_type`, `first_name`, `middle_name`, `last_name`, `email`, `phone`, `password_hash`, `picture`, `secret_key`) VALUES
 (3, 0, 'Danich', '', 'Hang', 'test@email.com', '51445559999', '$2y$10$m940Yn2vBnKdvdlXb6tNPuv67FFYVB2H/ejRZuvfhfmRC9oUtPHqq', 'blank.jpg', NULL),
 (4, 1, 'Jeremy ', '', 'Vison', 'jeremyvison@hotmail.com', '514555555555', '$2y$10$lwoxhY4Oy0O7zgyGZS5gGOfJe1QfJfM/zbzTWQT3sWnW/zYW1l/16', 'blank.jpg', NULL),
-(5, 0, 'Giuliana', '', 'Bouzon', 'test@gmail.com', '', '$2y$10$WNVhD/V6S4p1xJlnBuGGwOEA9eMXIaMWuSUKfTFrKvRqV34Qku.Au', 'blank.jpg', NULL),
-(6, 0, 'Test', '', 'LN', 'test2@gmail.com', '', '$2y$10$22OmPhpZJ9qGGPIqHP5SyudHGh1duP1SOlS3IQuW9A/lJfLjqB9x.', 'blank.jpg', NULL),
-(7, 1, 'Tarzan', '', 'Tarzan', 'tarzan@gmail.com', '', '$2y$10$AdsrMkKusrGe8E3j52SrGuTZc5gXGa1sHKJ4zMv4bUVVtkAfD/GAG', 'blank.jpg', NULL),
+(5, 0, 'Giuliana', '', 'Bouzon', 'test@gmail.com', '', '$2y$10$WNVhD/V6S4p1xJlnBuGGwOEA9eMXIaMWuSUKfTFrKvRqV34Qku.Au', 'blank.jpg', 'XEL4CGPHLVWBQZPL'),
+(6, 0, 'Test', '', 'LN', 'test2@gmail.com', '', '$2y$10$22OmPhpZJ9qGGPIqHP5SyudHGh1duP1SOlS3IQuW9A/lJfLjqB9x.', '6266f296e0d7c.png', NULL),
+(7, 1, 'Tarzan', '', 'Tarzan', 'tarzan@gmail.com', '', '$2y$10$AdsrMkKusrGe8E3j52SrGuTZc5gXGa1sHKJ4zMv4bUVVtkAfD/GAG', 'blank.jpg', 'JPIHUCAIUBKLV33N'),
 (8, 0, 'David', '', 'H', 'David@hotmail.com', '444444444', '$2y$10$DmBcPSHFHz.IyL7wZy3Xie9awxEPXOSfzTvu2CPSpoky1nbvP7y06', 'blank.jpg', NULL),
 (9, 1, 'Pizza', '', 'Owner', 'Pizza@gmail.c0m', '', '$2y$10$cI/U7cWgFMz9cYGHyCzUJexw2GVhjKjBvtd/R9C38OIQljHpBa6Dq', 'blank.jpg', NULL),
 (10, 0, 'pizza', '', 'pizzA', 'PIZAAA@email.com', '', '$2y$10$tdEjyULpOjETswvFCQJ40e8tKQ5NolJyP3AAcq4e44JkwZEAyERbK', 'blank.jpg', NULL),
@@ -219,7 +229,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `order`
 --
 ALTER TABLE `order`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT for table `order_detail`
@@ -243,7 +253,7 @@ ALTER TABLE `store`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- Constraints for dumped tables
@@ -253,8 +263,8 @@ ALTER TABLE `user`
 -- Constraints for table `order`
 --
 ALTER TABLE `order`
-  ADD CONSTRAINT `order_store_fk` FOREIGN KEY (`store_id`) REFERENCES `store` (`store_id`),
-  ADD CONSTRAINT `order_user_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+  ADD CONSTRAINT `order_store_fk` FOREIGN KEY (`store_id`) REFERENCES `store` (`store_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `order_user_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `order_detail`
