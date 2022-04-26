cdc28762393ab731a2a733281925ce897a83a90f
danichh <hangdanich@hotmail.com>
2022-04-26 05:37:51 -0400

    update the translation and fix view products

--- app/views/subviews/products.php
+++ app/views/subviews/products.php
@@ -1,4 +1,4 @@
-<h2 class = "text-center"><?=("Products:")?></h2>
+<h2 class = "text-center"><?=("Products:")?></h2><br><br>
 <div class="row" style='text-align:center;'>
     <?php
         if ($data != null) {
@@ -12,7 +12,7 @@
                             <p class=\"card-text\"> ". _("Description:") ." $product->product_description</p>";
 
                 if (isset($_SESSION['store_id']) && $_SESSION['store_id'] == $product->store_id) {
-                    echo "<a class=\"btn btn-primary\" href='/Product/update/$product->product_id' class='m-2'>". _("Update Product") ."</a>
+                    echo "<a class=\"btn btn-primary\" href='/Product/update/$product->product_id' class='m-2'>". _("Update Product") ."</a><br><br>
                         <a class=\"btn btn-primary\" href='/Product/delete/$product->product_id' onclick='return confirm(\"". _("Product successfully deleted") ."\")' class='m-2'>". _("Delete Product") ."</a></div>
                         </div></div>";
     
